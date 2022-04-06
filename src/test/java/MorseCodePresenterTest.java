4b80a438ec83b1603907ff768d4a966d6acba9e7
sarastavnitser <sstavnit@student.touro.edu>
2022-04-06 10:12:40 -0400

    started presenter test - has bugs

--- src/test/java/MorseCodePresenterTest.java
+++ src/test/java/MorseCodePresenterTest.java
@@ -1,17 +1,41 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MorseCodePresenterTest {
-    private MorseCodeFrame view;
-    private MorseCodeConverter model;
+    private MorseCodeFrame view = Mockito.mock(MorseCodeFrame.class);
+    private MorseCodeConverter model = Mockito.mock(MorseCodeConverter.class);
     private MorseCodePresenter presenter = new MorseCodePresenter(view, model);
+    private String morse = ".... . .-.. .-.. ---";
+    private String english = "hello";
 
     @Test
-    void toMorse() {
+    public void toMorse() {
+        //given
+        doReturn(morse).when(model).toMorse(english);
+
+        //when
+        presenter.toMorse(english);
+
+        //then
+        verify(model).toMorse(english);
+        verify(view).setToMorseLabel(morse);
     }
 
     @Test
     void toEnglish() {
+        //given
+        doReturn(english).when(model).toEnglish(morse);
+
+        //when
+        presenter.toEnglish(morse);
+
+        //then
+        verify(model).toEnglish(morse);
+        verify(view).setToEnglishLabel(english);
     }
 }
